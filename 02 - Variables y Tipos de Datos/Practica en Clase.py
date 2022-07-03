0490619f761481dcbfe3425893bea3c1c378562f
DonlYone1 <abel.uchupe@gmail.com>
2022-07-02 20:27:42 -0500

    first commit

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,11 +1,20 @@
 mi_variable = 12
 print(mi_variable)
 
+abelito_variable = 5
+print(abelito_variable)
+
 mi_variable2 = 'dario'
 print(mi_variable2)
 
 mi_complejo = 5 + 7j
 print(mi_complejo)
+
+a = 6
+b = 7
+c = int (input('Ingresar valor: '))
+
+print(a + b + c)
 
 
 mi_numero = input('Ingrese un valor')
